0db5f9be9f968abfd1ea60d9484233b9a121eec6
Austin <austin.goudge@psycle.com>
2015-04-30 12:05:40 +0100

    Added Game subclasses for Build, Parkour and TreasureHunt

---
+++ src/Psycle/SJTTournamentTools/game/Build.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Psycle\SJTTournamentTools\Game;
+
+/**
+ * Description of Parkour
+ *
+ * @author austin
+ */
+class TreasureHunt extends Game {
+    /**
+     * Constructor
+     *
+     * @param array $config The configuration array for the game (from the config file)
+     */
+    public function __construct($config) {
+        parent::__construct($config);
+    }
+}
